f848ff2bb8a260664ea5f65f74a8705ac55e85a2
En <graphic.mimage@gmail.com>
2025-07-16 13:56:55 +0700

    en/assign-1

--- ex-4.js
+++ ex-4.js
@@ -2,12 +2,17 @@
 50 + 12; // Expression
 
 //Exercise #4: What is Expression Start coding here
-130 + 300;
-let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
-typeof true;
-let name = "John";
-3 > 5;
-10 == 100;
-200;
+130 + 300; // Expression
+let zero = 1000 - 1000; // let ไม่เป็นแต่ 1000-1000 เป็น Expression
+20 * 120; // Expression
+1000 / 2; // Expression
+typeof true; // Expression
+
+// บรรทัด 10 นี้ John เป็น Expression เด้อ
+// เพราะ name เป็น String ไปอ่าค่าใน varible เจอ John อยู่ข้างใน จึงเป็น Expression
+
+let name = "John"; 
+
+3 > 5; // Expression
+10 == 100; // Expression
+200; // Expression 
